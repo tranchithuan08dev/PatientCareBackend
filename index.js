e0c4fa9b4bde8e0077d63bc6e61411530397afa7
ChiThuan <tranchithuanjr@gmail.com>
2025-01-13 16:59:40 +0700

    call dotenv

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
 const port = 5000;
+dotenv.config();
 app.get("/", function (req, res) {
   res.send("Hello World11111111121423423");
 });
 
 console.log(port);
 
-app.listen(port);
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
