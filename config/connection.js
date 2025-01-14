32ba20f6d632e2b4bf2b8ccaa3a931fdaa79449e
ChiThuan <tranchithuanjr@gmail.com>
2025-01-14 14:32:08 +0700

    connection database postgre

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,20 @@
+const { Pool } = require("pg");
 
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+});
+
+pool
+  .connect()
+  .then(() => {
+    console.log("Connected to the database successfully!");
+  })
+  .catch((err) => {
+    console.error("Connection failed: ", err.stack);
+  });
+
+module.exports = pool;
