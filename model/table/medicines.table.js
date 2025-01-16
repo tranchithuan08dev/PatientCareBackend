40d4ca14287fbaedfa1cbda7428db635ac643ace
ChiThuan <tranchithuanjr@gmail.com>
2025-01-16 17:39:02 +0700

    create table medicine

---
+++ model/table/medicines.table.js
@@ -0,0 +1,13 @@
+const medicinesTable = {
+  name: "medicines",
+  colums: {
+    medicineId: "medicineId",
+    nameMedicine: "nameMedicine",
+    description: "description",
+    priceIn: "priceIn",
+    priceOut: "priceOut",
+    quantity: "quantity",
+  },
+};
+
+module.exports = medicinesTable;
