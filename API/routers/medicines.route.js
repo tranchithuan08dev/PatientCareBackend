a0d4118f54cab8051acce6415d563b42827b1088
ChiThuan <tranchithuanjr@gmail.com>
2025-01-18 18:47:16 +0700

    create router getdetail medicines

--- API/routers/medicines.route.js
+++ API/routers/medicines.route.js
@@ -2,5 +2,6 @@ const medicinesController = require("../../API/controller/medicines.controller")
 const route = require("express").Router();
 
 route.get("/getAll", medicinesController.getAllMedicines);
+route.get("/detail", medicinesController.getDetailMedicines);
 
 module.exports = route;
