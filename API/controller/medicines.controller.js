448232fd46526baee66a30aa3af47bde4eb4ca4e
ChiThuan <tranchithuanjr@gmail.com>
2025-01-18 18:46:41 +0700

    create fuction get detail medicines

--- API/controller/medicines.controller.js
+++ API/controller/medicines.controller.js
@@ -2,6 +2,8 @@ const baseModel = require("../../model/base.model");
 const medicinesTable = require("../../model/table/medicines.table");
 const handleResponse = require("../../helper/handleRespone.helper");
 const handleError = require("../../helper/handleError.helper");
+const isValidId = require("../../validates/reqIdParam.validate");
+const handleRespone = require("../../helper/handleRespone.helper");
 const medicinesController = {
   getAllMedicines: async (req, res) => {
     let statusCode;
@@ -17,6 +19,36 @@ const medicinesController = {
       }
 
       return handleResponse(res, 200, { medicines: medicines });
+    } catch (error) {
+      return handleError(res, statusCode, error);
+    }
+  },
+
+  getDetailMedicines: async (req, res) => {
+    let statusCode = 500;
+    try {
+      const { id } = req.query;
+      if (!id) {
+        statusCode = 400;
+        throw new Error("Invalid ID");
+      }
+
+      const medicinesDetail = await baseModel.findById(
+        medicinesTable.name,
+        medicinesTable.colums.medicineId,
+        id
+      );
+
+      console.log("medicinesDetail", medicinesDetail);
+
+      if (!medicinesDetail) {
+        statusCode = 404;
+        throw new Error("Medicine details not found");
+      }
+
+      return handleRespone(res, 200, {
+        data: medicinesDetail,
+      });
     } catch (error) {
       return handleError(res, statusCode, error);
     }
