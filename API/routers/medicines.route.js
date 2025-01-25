a906b362fe79613e48291fb1499418e0c01984a3
ChiThuan <tranchithuanjr@gmail.com>
2025-01-25 20:31:58 +0700

    create router update and create

--- API/routers/medicines.route.js
+++ API/routers/medicines.route.js
@@ -3,5 +3,6 @@ const route = require("express").Router();
 
 route.get("/getAll", medicinesController.getAllMedicines);
 route.get("/detail", medicinesController.getDetailMedicines);
-
+route.post("/create", medicinesController.createMedicine);
+route.patch("/update", medicinesController.updateMedicine);
 module.exports = route;
