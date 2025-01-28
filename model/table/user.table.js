8259e4d74e2662d39125912f480c894220db64d4
ChiThuan <tranchithuanjr@gmail.com>
2025-01-28 22:05:54 +0700

    create table user

---
+++ model/table/user.table.js
@@ -0,0 +1,14 @@
+const userTable = {
+  name: "users",
+  columns: {
+    userId: "userid",
+    fullName: "fullname",
+    gender: "gender",
+    dateOfBirth: "dateofbirth",
+    phoneNumber: "phonenumber",
+    address: "address",
+    role: "role",
+  },
+};
+
+module.exports = userTable;
