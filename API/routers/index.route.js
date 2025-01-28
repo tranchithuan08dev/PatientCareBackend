4bc067900a45e67ddf8d141744acec298d513275
ChiThuan <tranchithuanjr@gmail.com>
2025-01-28 23:04:45 +0700

    create router usertable

--- API/routers/index.route.js
+++ API/routers/index.route.js
@@ -1,5 +1,6 @@
 const medicinesRouter = require("../routers/medicines.route");
-
+const userRouter = require("../routers/user.route");
 module.exports = (app) => {
   app.use("/api/medicines", medicinesRouter);
+  app.use("/api/user", userRouter);
 };
