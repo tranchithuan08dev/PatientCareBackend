c8a832e3b0446391d94a1f2711909995065b1be3
ChiThuan <tranchithuanjr@gmail.com>
2025-01-30 23:02:40 +0700

    create router diagnosis

--- API/routers/index.route.js
+++ API/routers/index.route.js
@@ -1,6 +1,8 @@
 const medicinesRouter = require("../routers/medicines.route");
 const userRouter = require("../routers/user.route");
+const diagnosisRouter = require("../routers/diagnosis.route");
 module.exports = (app) => {
   app.use("/api/medicines", medicinesRouter);
   app.use("/api/user", userRouter);
+  app.use("/api/diagnosis", diagnosisRouter);
 };
