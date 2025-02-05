727b5d9e8be3c773bb790b63fd690a2db739b6a4
ChiThuan <tranchithuanjr@gmail.com>
2025-02-05 15:21:57 +0700

    create get all user

--- API/routers/user.route.js
+++ API/routers/user.route.js
@@ -2,4 +2,5 @@ const userController = require("../../API/controller/user.controller");
 const route = require("express").Router();
 
 route.post("/create", userController.create);
+route.get("/getAll", userController.getAll);
 module.exports = route;
