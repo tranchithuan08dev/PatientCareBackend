730710970268e75a648ad0088b6a697a7e6d6454
ChiThuan <tranchithuanjr@gmail.com>
2025-02-06 11:54:50 +0700

    create router get detail

--- API/routers/user.route.js
+++ API/routers/user.route.js
@@ -3,4 +3,5 @@ const route = require("express").Router();
 
 route.post("/create", userController.create);
 route.get("/getAll", userController.getAll);
+route.get("/getdetail", userController.getDetail);
 module.exports = route;
