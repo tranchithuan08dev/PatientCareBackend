232b70f88b96d0ced4c750fb254d952d4fef1d1c
ChiThuan <tranchithuanjr@gmail.com>
2025-02-06 19:09:02 +0700

    create router update users

--- API/routers/user.route.js
+++ API/routers/user.route.js
@@ -2,6 +2,7 @@ const userController = require("../../API/controller/user.controller");
 const route = require("express").Router();
 
 route.post("/create", userController.create);
+route.post("/update", userController.update);
 route.get("/getAll", userController.getAll);
 route.get("/getdetail", userController.getDetail);
 module.exports = route;
