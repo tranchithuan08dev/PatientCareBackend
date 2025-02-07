156748620417775aeda2d25df8c8855928817d8b
ChiThuan <tranchithuanjr@gmail.com>
2025-02-07 15:29:32 +0700

    create router get detail

--- API/routers/diagnosis.route.js
+++ API/routers/diagnosis.route.js
@@ -2,4 +2,5 @@ const diagnosisController = require("../../API/controller/diagnosis.controller")
 const route = require("express").Router();
 
 route.post("/create", diagnosisController.create);
+route.get("/getdetail", diagnosisController.getDetail);
 module.exports = route;
