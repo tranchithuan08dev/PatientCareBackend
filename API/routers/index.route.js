5289031a70d44fa55f10e3e7031b97020c281c04
ChiThuan <tranchithuanjr@gmail.com>
2025-02-10 11:53:52 +0700

    add route payment

--- API/routers/index.route.js
+++ API/routers/index.route.js
@@ -1,8 +1,10 @@
 const medicinesRouter = require("../routers/medicines.route");
 const userRouter = require("../routers/user.route");
 const diagnosisRouter = require("../routers/diagnosis.route");
+const paymentRouter = require("../routers/payment.route");
 module.exports = (app) => {
   app.use("/api/medicines", medicinesRouter);
   app.use("/api/user", userRouter);
   app.use("/api/diagnosis", diagnosisRouter);
+  app.use("/api/payment", paymentRouter);
 };
