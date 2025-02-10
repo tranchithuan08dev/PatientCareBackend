0710a9af7b717e9c6043236f902e906a9f5380a2
ChiThuan <tranchithuanjr@gmail.com>
2025-02-10 11:55:26 +0700

    create table payment

---
+++ model/table/payment.table.js
@@ -0,0 +1,11 @@
+const paymentTable = {
+  name: "payment",
+  columns: {
+    paymentId: "paymentid",
+    diagnosisId: "diagnosisid",
+    paymentDate: "paymentdate",
+    amount: "amount",
+  },
+};
+
+module.exports = paymentTable;
